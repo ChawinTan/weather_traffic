ae4fd15428555e6baaa8940d878652152acc603d
ChawinTan <lowsesbbfa@gmail.com>
2024-01-23 19:46:51 +0800

    add more unit test

--- weather-app/server/src/app.controller.spec.ts
+++ weather-app/server/src/app.controller.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
+
+const httpService = {
+  get: jest.fn(),
+  post: jest.fn().mockImplementation(() => of({ data: {} })),
+};
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +15,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: HttpService,
+          useValue: httpService
+        }
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
--- weather-app/src/locations/index.test.tsx
+++ weather-app/src/locations/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen  } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor  } from '@testing-library/react';
 import Locations from '.';
-
+import fetch from 'jest-fetch-mock'
 const locations = ['area'];
 const traffic = [{  lat: '1.11',lon: '2.11', imgUrl: 'http://google.com'}];
 
@@ -10,4 +10,18 @@ test('should render properly', async () => {
   render(<Locations traffic={traffic} location={locations} />);
   expect(screen.getByText('area')).toBeInTheDocument()
 });
+
+test('should show weather when click', async () => {
+  fetch.mockImplementation(() => Promise.resolve({ json: () => ({area: 'area', forecast: 'forecast'}) }) as any)
+  render(<Locations traffic={traffic} location={locations} />);
+
+  expect(screen.getByText('area')).toBeInTheDocument()
+
+  const getAreaButton = screen.getByText('area');
+  fireEvent.click(getAreaButton);
+
+  await waitFor(() => {
+    expect(screen.getByText('Area - area')).toBeInTheDocument();
+  })
+})
 
